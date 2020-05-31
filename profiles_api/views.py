ff05bca4c5bf165a3bdb12d5d81684b7703aaefd
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-05-31 14:38:31 +0530

    post, put, patch and delete placeholder methods implementation

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-
-    def get(self,request,format=None):
+    def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
             'Users HTTP methods as functions (get, post, patch, put, delete)',
@@ -16,5 +17,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self, request, format=None):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """Handle partial update an object"""
+        return Response({'method':'PATCH'})
+
+
+    def delete(self, request, pk=None):
+        """Delete an Object"""
+        return Response({'method': 'DELETE'})
 
 
